03968c5d312176b843d279c09a60e4ffc26f93a7
Juan A. Garcia Pardo <juagargi@gmail.com>
2022-10-07 11:14:16 +0200

    Colibri gateway as a service with configuration.
    Follow the regular scionproto idiom to start the new service.

--- go/coligate/main.go
+++ go/coligate/main.go
@@ -14,8 +14,55 @@
 
 package main
 
-import "fmt"
+import (
+	"context"
+
+	"github.com/scionproto/scion/go/lib/log"
+	"github.com/scionproto/scion/go/lib/serrors"
+	"github.com/scionproto/scion/go/lib/topology"
+	"github.com/scionproto/scion/go/pkg/app"
+	"github.com/scionproto/scion/go/pkg/app/launcher"
+	"github.com/scionproto/scion/go/pkg/coligate/config"
+	"golang.org/x/sync/errgroup"
+)
 
 func main() {
-	fmt.Println("hello world")
+	var cfg config.Config
+	application := launcher.Application{
+		TOMLConfig: &cfg,
+		ShortName:  "SCION COLIBRI Gateway",
+		Main: func(ctx context.Context) error {
+			return realMain(ctx, &cfg)
+		},
+	}
+	application.Run()
+}
+
+func realMain(ctx context.Context, cfg *config.Config) error {
+	topo, err := topology.NewLoader(topology.LoaderCfg{
+		File:      cfg.General.Topology(),
+		Reload:    app.SIGHUPChannel(ctx),
+		Validator: &topology.ColibriValidator{ID: cfg.General.ID},
+		// Metrics: , // TODO(justin) add observability to the gateway
+	})
+	if err != nil {
+		return serrors.WrapStr("creating topology loader", err)
+	}
+	g, errCtx := errgroup.WithContext(ctx)
+	g.Go(func() error {
+		defer log.HandlePanic()
+		return topo.Run(errCtx)
+	})
+
+	var cleanup app.Cleanup
+	// TODO(justin) setup gateway and start running both the control plane and
+	// the data plane here. TAL at go/co or go/cs for inspiration.
+
+	// cleanup when exit
+	g.Go(func() error {
+		defer log.HandlePanic()
+		<-errCtx.Done()
+		return cleanup.Do()
+	})
+	return g.Wait()
 }
