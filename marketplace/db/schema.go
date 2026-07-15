58fa1e4cd2909c2f3a49437ace6c2386c32c5ae7
rohrerj <26304001+rohrerj@users.noreply.github.com>
2026-07-15 15:35:07 +0200

    added script to preload assets, users, reservations and redemption delegations into marketplace db

--- marketplace/db/schema.go
+++ marketplace/db/schema.go
@@ -14,73 +14,13 @@
 
 package db
 
+import (
+	_ "embed"
+)
+
 const (
 	SchemaVersion = 1
-	Schema        = `
-	CREATE TABLE Assets(
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		owner_id INTEGER,
-		isd_id INTEGER NOT NULL,
-		as_id INTEGER NOT NULL,
-		bandwidth INTEGER NOT NULL,
-		bandwidth_min INTEGER NOT NULL,
-		bandwidth_max INTEGER NOT NULL CHECK (bandwidth_max >= bandwidth_min),
-		price INTEGER NOT NULL,
-		time_granularity INTEGER NOT NULL,
-		time_min_duration INTEGER NOT NULL,
-		starts_at TEXT NOT NULL,
-		stops_at TEXT NOT NULL,
-		ingress INTEGER,
-		egress INTEGER,
-		state INTEGER NOT NULL DEFAULT 0,
-		FOREIGN KEY (owner_id) REFERENCES Users(id)
-	);
-	CREATE INDEX idx_asset_owner ON Assets(owner_id);
-	CREATE INDEX idx_asset_validity ON Assets(starts_at, stops_at);
-	CREATE INDEX idx_assets_ia_validity_start ON Assets(isd_id, as_id, starts_at, stops_at);
-	CREATE INDEX idx_assets_ia_validity_end ON Assets(isd_id, as_id, stops_at, starts_at);
-	CREATE TABLE Reservations(
-		id INTEGER NOT NULL,
-		owner_id INTEGER NOT NULL,
-		isd_id INTEGER NOT NULL,
-		as_id INTEGER NOT NULL,
-		ingress INTEGER NOT NULL,
-		egress INTEGER NOT NULL,
-		bandwidth INTEGER NOT NULL,
-		bw_encoded INTEGER NOT NULL,
-		starts_at TEXT NOT NULL,
-		stops_at TEXT NOT NULL,
-		key BLOB NOT NULL,
-		FOREIGN KEY (owner_id) REFERENCES Users(id)
-	);
-	CREATE INDEX idx_reservations_owner ON Reservations(owner_id);
-	CREATE INDEX idx_reservations_used ON Reservations(isd_id, as_id, starts_at, stops_at);
-	CREATE TABLE Users(
-		id INTEGER PRIMARY KEY,
-		name TEXT NOT NULL UNIQUE,
-		pw_hash TEXT NOT NULL,
-		jwt_version INTEGER NOT NULL DEFAULT 0,
-		balance INTEGER NOT NULL DEFAULT 0 CHECK (balance >= 0)
-	);
-	CREATE INDEX idx_users_name ON Users(name);
-	CREATE TABLE Ases(
-		isd_id INTEGER NOT NULL,
-		as_id INTEGER NOT NULL,
-		pw_hash TEXT NOT NULL DEFAULT '',
-		jwt_version INTEGER NOT NULL DEFAULT 0,
-		balance INTEGER NOT NULL DEFAULT 0 CHECK (balance >= 0),
-		PRIMARY KEY(isd_id, as_id)
-	);
-	CREATE TABLE Redemption_Delegations(
-		isd_id INTEGER NOT NULL,
-		as_id INTEGER NOT NULL,
-		res_id_limit INTEGER NOT NULL,
-		expiration TEXT NOT NULL CHECK (expiration <= paid_until),
-		paid_until TEXT NOT NULL,
-		key BLOB NOT NULL,
-		encodings BLOB NOT NULL,
-		PRIMARY KEY(isd_id, as_id)
-	);
-	CREATE INDEX idx_redemption_expiration ON Redemption_Delegations(expiration);
-	`
 )
+
+//go:embed schema.sql
+var Schema string
